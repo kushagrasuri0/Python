faf769294f9cc0a4d7cefabe2b47c5813fef0cd6
kushagrasuri0 <kushagral.24.beai@acharya.ac.in>
2025-05-24 22:18:18 +0530

    Create CharacterPictureGrid.py

---
+++ CharacterPictureGrid.py
@@ -0,0 +1,8 @@
+print("Name: Kushagra Suri")
+print("USN: 1AY24AI058")
+print("Section: M")
+grid_size = int(input("Enter the size of the grid (for example 6 for 6X6: "))
+for i in range(grid_size):
+    for j in range(grid_size):
+        print("*", end=" ")
+    print()
