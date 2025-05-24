13fd6bd35b6f600696e25ef161c6a429aec516f4
kushagrasuri0 <kushagral.24.beai@acharya.ac.in>
2025-05-24 22:49:44 +0530

    Create TablePrinter.py

---
+++ TablePrinter.py
@@ -0,0 +1,6 @@
+print("Name: Kushagra Suri")
+print("USN: 1AY24AI058")
+print("Section: M")
+number = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(f"{number} x {i} = {number * i}")
